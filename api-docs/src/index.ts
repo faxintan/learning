93529d15a0c10a649d00c56b5b4963e30e7974b4
faxintan <faxintan@foxmail.com>
2020-12-28 20:05:44 +0800

    Create index.ts

---
+++ api-docs/src/index.ts
@@ -0,0 +1,19 @@
+
+/**
+ * @packageDocumentation
+ * 返回 hello 开头的字符串
+ * @param str - input string
+ * @returns 'hello xxx'
+ * @example
+ * ```ts
+ * myFirstFunc('ts') => 'hello ts'
+ * ```
+ * 
+ * @beta
+ */
+
+function myFirstFunc(str: string): string {
+	return str
+}
+
+export default myFirstFunc
